63366d9fe74c0fd9e9f7992829922559760ddb47
victorpreston <prestonvictor25@gmail.com>
2024-02-16 05:42:12 +0300

    [v1]: analytics-dashboard

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,5 +1,6 @@
 import { redis } from '@/lib/redis';
 import { getDate } from '@/utils';
+import { parse } from 'date-fns'
 
 type AnalyticsArgs = {
     retention: number;
@@ -27,16 +28,44 @@ export class Analytics {
         if (opts?.persist) await redis.expire(key, this.retention);
     }
 
-    async  retrieve(namespace: string) {
-        const res = await redis.hgetall<Record<string, string>>('analytics::${namespace}::${date}');
-
-        return {
-            date,
-            events: Object.entries(res ?? []).map(([key, value]) => ({
-                [key]: Number(value)
-            }))
+    async retrieveDays(namespace: string, nDays: number) {
+        type AnalyticsPromise = ReturnType<typeof analytics.retrieve>
+        const promises: AnalyticsPromise[] = []
+    
+        for (let i = 0; i < nDays; i++) {
+          const formattedDate = getDate(i)
+          const promise = analytics.retrieve(namespace, formattedDate)
+          promises.push(promise)
         }
-    }
+    
+        const fetched = await Promise.all(promises)
+    
+        const data = fetched.sort((a, b) => {
+          if (
+            parse(a.date, 'dd/MM/yyyy', new Date()) >
+            parse(b.date, 'dd/MM/yyyy', new Date())
+          ) {
+            return 1
+          } else {
+            return -1
+          }
+        })
+    
+        return data
+      }
+
+    async retrieve(namespace: string, date: string) {
+        const res = await redis.hgetall<Record<string, string>>(
+          `analytics::${namespace}::${date}`
+        )
+    
+        return {
+          date,
+          events: Object.entries(res ?? []).map(([key, value]) => ({
+            [key]: Number(value),
+          })),
+        }
+      }
 
 
 }
